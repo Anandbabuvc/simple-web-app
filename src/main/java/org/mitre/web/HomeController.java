313ff1163dddba691a3c38e9e55fd0e08db17ae8
Anandbabuvc <48248919+Anandbabuvc@users.noreply.github.com>
2020-01-12 12:21:09 +0530

    Update HomeController.java

--- src/main/java/org/mitre/web/HomeController.java
+++ src/main/java/org/mitre/web/HomeController.java
@@ -30,9 +30,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+/**
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+*/
 
 /**
  * Handles requests for the application home page.
